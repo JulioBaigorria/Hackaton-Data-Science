8da95e35b5d45813bfd596ee407f2d129f10f308
JULIO BAIGORRIA <julio.baigorria1@gmail.com>
2024-03-16 19:35:00 -0300

    #Mejora de Presentacion

--- readme.md
+++ readme.md
@@ -1,43 +1,14 @@
 # Trabajo para Hackaton de Ciencia de Datos.
 
-## Fuente Principal: https://archive.ics.uci.edu/dataset/17/breast+cancer+wisconsin+diagnostic
+## Tema: 
+Prediccion de Diagnóstico de Cáncer de Mamas, obtenidas digitalmente mediante la citología por Punción Aspiración con Aguja Fina (PAFF).
 
-## Paper: [NUCLEAR FEATURE EXTRACTION FOR BREAST TUMOR DIAGNOSIS](https://minds.wisconsin.edu/bitstream/handle/1793/59692/TR1131.pdf;jsessionid=A17A68C4112D52DF0267D965E7522D75?sequence=1)
+## Fuentes:
+**Paper**: [NUCLEAR FEATURE EXTRACTION FOR BREAST TUMOR DIAGNOSIS](https://minds.wisconsin.edu/bitstream/handle/1793/59692/TR1131.pdf;jsessionid=A17A68C4112D52DF0267D965E7522D75?sequence=1)
 
-**Tema:** Detección de Cáncer de Mamas a travez de muestras multidimencionales, obtenidas digitalmente mediante la técnica y procedimiento de Biopsia por aspiración con aguja fina.
-
-**Datos:** La fuente de datos tomada representa cada muestra obtenida con el diagnóstico (Benigno - Maligno) y atributos de la muestra obtenida siendo Radio, Textura, Perimetro, Área, Suavidad, Compacidad, Concavidad, Puntos Concavos, Simetria y Dimension Fractal.
-
-El plano de separación que se utiliza para predecir la presencia o ausencia de cáncer de mama se obtuvo utilizando el árbol del método multisuperficie (MSM-T), un método de clasificación que utiliza programación lineal para construir un árbol de decisión.
-
-El MSM-T funciona buscando un conjunto de características que puedan separar los datos de cáncer de los datos de no cáncer. Para ello, el MSM-T realiza una búsqueda exhaustiva en el espacio de 1 a 4 características y 1 a 3 planos de separación.
-
-**Observaciones:**
-
-Archivos:
-descripcion.csv -> Descripción del Dataset (Cuenta, Media, Desvío estándar, Minimo, Media, etc.)
-correlaciones_b.csv -> Correlaciones de la categoría B
-correlaciones_m.csv -> Correlaciones de la categoría M
-correlaciones.csv -> Correlaciones del Dataset completo.
-wdbc.csv -> Dataset con las muestras.
-wdbc.xlsx -> Dataset con las muestras en excel en caso de necesitarse.
+**Fuente de Datos:** [Breast Cancer Wisconsin (Diagnostic)](https://archive.ics.uci.edu/dataset/17/breast+cancer+wisconsin+diagnostic)
 
 
-1. Responder: ¿Qué tienen en común los datos de cada tipo de diagnóstico? ¿Qué los diferencia de los Malignos y Benignos?
-2. Graficar histogramas para revisar la distribución de los distintos features.
-3. Graficar con un dotplot para estudiar si hay alguna tendencia.
-4. Graficar con Boxplot.
+## Entregables:
 
-**Objetivo:**
-
-- Leer el Paper. Lo primero que deberíamos hacer todos para entender sobre el asunto. Está en inglés.
-- Realizar análisis exploratorio para explicar los casos.
-- Redactar un Paper entregable.
-- Convenir qué parte expondrá cada uno.
-- Armar un modelo predictor. Máximo 2.
-
-
-
-
-
-
+**Documentación:** [Tema: Prediccion de Diagnóstico de Cáncer de Mamas ](https://docs.google.com/document/d/1cZO8qYvFg7bBAjin5wEtbb_azxczRfSPh5NjToAlRyg/edit?usp=sharing)
--- script.py
+++ script.py
@@ -1,22 +1,196 @@
 # Importar librerías
-
-import numpy as np
 import pandas as pd
-import matplotlib as mlt
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.svm import SVC
+from imblearn.over_sampling import SMOTE
+
+############## LEER DOCUMENTO
 
 df_wdbc = pd.read_csv('wdbc.csv', sep=',')
 
-df_wdbc['ID'] = df_wdbc['ID'].astype('object')
+############## BALANCE DEL DATAFRAME REALIZANDO OVERSAMPLING
 
-df_wdbc.describe().T
+X = df_wdbc.drop(columns=['ID', 'Diagnosis'])  # Características
+y = df_wdbc['Diagnosis']  # Etiquetas
 
-df_wdbc.info()
+# Crear una instancia de SMOTE
+smote = SMOTE()
 
-df_wdbc.astype(object).describe().T
+# Aplicar SMOTE para generar muestras sintéticas
+X_oversampled, y_oversampled = smote.fit_resample(X, y)
+
+# Unir las características y las etiquetas sintéticas en un nuevo DataFrame
+df_oversampled = pd.concat([pd.DataFrame(X_oversampled, columns=X.columns),
+                            pd.DataFrame(y_oversampled, columns=['Diagnosis'])],
+                            axis=1)
+
+
+############## CORRELACION POR GRUPO
+
+FLOAT_COLUMNS: list[str] = ['radius1', 'texture1', 'perimeter1', 'area1',
+                            'smoothness1', 'compactness1', 'concavity1', 'concave_points1',
+                            'symmetry1', 'fractal_dimension1', 'radius2', 'texture2', 'perimeter2',
+                            'area2', 'smoothness2', 'compactness2', 'concavity2', 'concave_points2',
+                            'symmetry2', 'fractal_dimension2', 'radius3', 'texture3', 'perimeter3',
+                            'area3', 'smoothness3', 'compactness3', 'concavity3', 'concave_points3',
+                            'symmetry3', 'fractal_dimension3']
+
+df_wdbc_m = df_wdbc[df_wdbc['Diagnosis'] == 'M']
+
+df_wdbc_b = df_wdbc[df_wdbc['Diagnosis'] == 'B']
+
+df_wdbc_m[FLOAT_COLUMNS].corr().to_csv('docs/correlaciones_m.csv', sep=',')
+
+df_wdbc_b[FLOAT_COLUMNS].corr().to_csv('docs/correlaciones_b.csv', sep=',')
+
+
+############## REGRESIÓN LOGÍSTICA
+
+# Seleccionar las variables predictoras: radius3 y texture3.
+X = df_oversampled[['radius3', 'texture3']]
+
+# Seleccionar la variable objetivo: 'Diagnosis'.
+y = df_oversampled['Diagnosis']
+
+# Dividir el conjunto de datos en conjuntos de entrenamiento y prueba, usando un margen de 0.2 reservado.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=40)
+
+# Inicializar y entrenar el modelo de Regresión Logística.
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Hacer predicciones en el conjunto de prueba.
+y_pred = model.predict(X_test)
+
+# Evaluar el rendimiento del modelo.
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+
+# Imprimir la matriz de confusión y el informe de clasificación.
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=model.classes_, yticklabels=model.classes_)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Matriz de Confusión')
+plt.show()
+
+print('\nInforme de Clasificación:')
+print(classification_report(y_test, y_pred))
+
+
+############## K N N
+
+# Seleccionar variables predictoras
+X = df_oversampled.drop(['ID', 'Diagnosis'], axis=1)
+
+# Seleccionar la variable objetivo
+y = df_oversampled['Diagnosis']
+
+# Dividir el conjunto de datos en conjuntos de entrenamiento y prueba. Ajustado Random State = 60
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=40)
+
+# Inicializar el modelo KNN, ajustando K = 6
+knn_model = KNeighborsClassifier(n_neighbors = 4 )
+
+# Entrenar el modelo KNN
+knn_model.fit(X_train, y_train)
+
+# Hacer predicciones en el conjunto de prueba
+y_pred = knn_model.predict(X_test)
+
+# Evaluar el rendimiento del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+
+# Imprimir la matriz de confusión y el informe de clasificación
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=knn_model.classes_, yticklabels=knn_model.classes_)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Matriz de Confusión')
+plt.show()
+
+print('\nInforme de Clasificación:')
+print(classification_report(y_test, y_pred))
+
+
+############## S V C
+
+# Seleccionar variables predictoras
+
+X = df_oversampled[['radius3', 'texture3']]
+# Seleccionar la variable objetivo
+y = df_oversampled['Diagnosis']
+
+# Dividir el conjunto de datos en conjuntos de entrenamiento y prueba.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=40)
+
+# Inicializar el modelo SVC
+svc_model = SVC(kernel='linear')
+
+# Entrenar el modelo SVC
+svc_model.fit(X_train, y_train)
+
+# Hacer predicciones en el conjunto de prueba
+y_pred = svc_model.predict(X_test)
+
+# Evaluar el rendimiento del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+
+# Imprimir la matriz de confusión y el informe de clasificación
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=svc_model.classes_, yticklabels=svc_model.classes_)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Matriz de Confusión')
+plt.show()
+
+print('\nInforme de Clasificación:')
+print(classification_report(y_test, y_pred))
+
+
+############## REGRESIÓN LINEAL
+
+X = df_oversampled[['perimeter1']]
+
+# Seleccionar la variable objetivo
+y = df_oversampled['concave_points1']
+
+# Dividir el conjunto de datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=40)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Hacer predicciones en el conjunto de prueba
+y_pred = model.predict(X_test)
+
+# Visualizar los resultados
+plt.scatter(X_test, y_test, color='black', label='Datos reales')
+plt.plot(X_test, y_pred, color='blue', linewidth=3, label='Regresión lineal')
+plt.xlabel('perimeter1')
+plt.ylabel('concave_points1')
+plt.legend()
+plt.title('Regresión Lineal entre perimeter1 y concave_points1')
+plt.show()
+
+sns.scatterplot(x='compactness1', y='concavity1', hue='Diagnosis', data=df_wdbc, palette='viridis')
+plt.xlabel('compactness1')
+plt.ylabel('concavity1')
+plt.title('Scatter Plot entre compactness1 y concavity1 (coloreado por Diagnosis)')
+plt.show()
+
+
+############## PLOTS
 
 # Generar las imagenes de Boxplot.
+
 sns.boxplot(x='Diagnosis', y='radius1', data=df_wdbc)
 sns.boxplot(x='Diagnosis', y='radius2', data=df_wdbc)
 sns.boxplot(x='Diagnosis', y='radius3', data=df_wdbc)
@@ -36,6 +210,10 @@ sns.boxplot(x='Diagnosis', y='area3', data=df_wdbc)
 sns.boxplot(x='Diagnosis', y='smoothness1', data=df_wdbc)
 sns.boxplot(x='Diagnosis', y='smoothness2', data=df_wdbc)
 sns.boxplot(x='Diagnosis', y='smoothness3', data=df_wdbc)
+
+sns.boxplot(x='Diagnosis', y='compactness1', data=df_wdbc)
+sns.boxplot(x='Diagnosis', y='compactness2', data=df_wdbc)
+sns.boxplot(x='Diagnosis', y='compactness3', data=df_wdbc)
 
 sns.boxplot(x='Diagnosis', y='concavity1', data=df_wdbc)
 sns.boxplot(x='Diagnosis', y='concavity2', data=df_wdbc)
@@ -69,21 +247,3 @@ selected_vars = df_wdbc.drop('ID', axis=1)
 sns.pairplot(selected_vars, hue='Diagnosis')
 plt.suptitle('Pairplot de variables seleccionadas', y=1.02)
 plt.show()
-
-
-# Revisión de Correlaciones por grupo. 
-FLOAT_COLUMNS: list[str] = ['radius1', 'texture1', 'perimeter1', 'area1',
-                            'smoothness1', 'compactness1', 'concavity1', 'concave_points1',
-                            'symmetry1', 'fractal_dimension1', 'radius2', 'texture2', 'perimeter2',
-                            'area2', 'smoothness2', 'compactness2', 'concavity2', 'concave_points2',
-                            'symmetry2', 'fractal_dimension2', 'radius3', 'texture3', 'perimeter3',
-                            'area3', 'smoothness3', 'compactness3', 'concavity3', 'concave_points3',
-                            'symmetry3', 'fractal_dimension3']
-
-df_wdbc_m = df_wdbc[df_wdbc['Diagnosis'] == 'M']
-
-df_wdbc_b = df_wdbc[df_wdbc['Diagnosis'] == 'B']
-
-df_wdbc_m[FLOAT_COLUMNS].corr().to_csv('docs/correlaciones_m.csv', sep=',')
-
-df_wdbc_b[FLOAT_COLUMNS].corr().to_csv('docs/correlaciones_b.csv', sep=',')
